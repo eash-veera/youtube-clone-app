a0ceacb6800b4e8c5b895f8a6a2b652da1e1d7d9
eash-veera <eshwaranveerabahu.biz@gmail.com>
2019-11-04 17:41:27 +0530

    rating and video medta data components have been updated, with the tests added !

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,13 +2,14 @@ import React,{ Component } from 'react';
 import './Watch.scss';
 import { Video } from '../../components/Video/Video';
 import { RelatedVideos } from '../../components/RelatedVideos/RelatedVideos';
+import { VideoMetadata } from '../../components/VideoMetadata/VideoMetadata';
 
 export class Watch extends Component{
     render(){
         return (
             <div className='watch-grid'>
                 <Video className='video' id='7fuHEEmEjs' />
-                <div className='metadata' style={{width: '100%', height:'100px', background:'#F91112'}}>Metadata</div>
+                <VideoMetadata className='metadata' viewCount={1000}/>
                 <div className='video-info-box' style={{width:'100%', height:'100px', background:'#BD10E0'}}>Video Info Box</div> 
                 <div className='comments' style={{width:'100%', height:'100px', background:'#9013FE'}}>Comments</div>
                 <RelatedVideos className='relatedVideos'/>
