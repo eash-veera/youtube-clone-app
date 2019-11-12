a49ac25fac04cc302c2bf5fefadd94eabe078eb5
eash-veera <eshwaranveerabahu.biz@gmail.com>
2019-11-13 01:29:03 +0530

    added comment section, imporved test methodology

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -3,6 +3,8 @@ import './Watch.scss';
 import { Video } from '../../components/Video/Video';
 import { RelatedVideos } from '../../components/RelatedVideos/RelatedVideos';
 import { VideoMetadata } from '../../components/VideoMetadata/VideoMetadata';
+import { VideoInfoBox } from '../../components/VideoInfoBox/VideoInfoBox';
+import { Comments } from '../Comments/Comments';
 
 export class Watch extends Component{
     render(){
@@ -10,8 +12,8 @@ export class Watch extends Component{
             <div className='watch-grid'>
                 <Video className='video' id='7fuHEEmEjs' />
                 <VideoMetadata className='metadata' viewCount={1000}/>
-                <div className='video-info-box' style={{width:'100%', height:'100px', background:'#BD10E0'}}>Video Info Box</div> 
-                <div className='comments' style={{width:'100%', height:'100px', background:'#9013FE'}}>Comments</div>
+                <VideoInfoBox className='video-info-box'/>
+                <Comments className='comments'/>
                 <RelatedVideos className='relatedVideos'/>
             </div>
         );
